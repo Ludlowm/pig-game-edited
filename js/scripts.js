7dc54708ad0f306d7f1d449a8bd971e4e8f73dd7
Marko Ludlow and Nick Powell <marko@>
2017-04-05 14:57:03 -0700

    add seperate hold button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 //Business Logic
 function player(userRoll, playerTotal) {
-this.userRoll = roll;
+this.userRoll = [];
 this.playerTotal = total;
 }
 
-function diceRoll() {
+function diceRoll(currentRoll) {
   return Math.floor(Math.random() * 6 + 1);
 }
 
@@ -13,21 +13,22 @@ var rollArray = [];
 
 
 
-
-
-
 //User Interface Logic
 $(document).ready(function(){
+
   $("#roll").click(function() {
-    var currentRoll = diceRoll();
+    var currentRoll = diceRoll(currentRoll);
     rollArray.push(currentRoll);
+
+    if (currentRoll === 1) {
+      rollArray = [];
+    }
     var sum = rollArray.reduce((a, b) => a + b, 0);
 
-
-      var roll = currentRoll ++
     $("#output").text(sum);
+    $("#hold").click(function() {
+      $("#totalOutput").text(sum)
     console.log(currentRoll);
-
-
+  });
   });
 });
