18b1a09895adb4e2a72ebdcdf1c660f383614aa6
Marko Ludlow <ludlow.marko@gmail.com>
2017-04-05 22:33:06 -0700

    add new pig dice game using constructors and objects. with help of friend.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,72 @@
 //Business Logic
-function player(userRoll, playerTotal) {
-this.userRoll = [];
-this.playerTotal = total;
+function player(n, tot) {
+this.name = n;
+this.tot = tot;
+this.rollArray = [];
 }
 
 function diceRoll(currentRoll) {
   return Math.floor(Math.random() * 6 + 1);
 }
 
-var rollArray = [];
+
+var curSum;
+var currentPlayer = 0;
+var playerCount = 2;
 
 
-
+function gameboy(){
+    this.players = [];
+    for (i = 0; i < playerCount; i++){
+      this.players[i] = new player("(none-set)", 0);
+    }
+}
 
 //User Interface Logic
 $(document).ready(function(){
+  var game = new gameboy();
 
   $("#roll").click(function() {
     var currentRoll = diceRoll(currentRoll);
-    rollArray.push(currentRoll);
+    game.players[currentPlayer].rollArray.push(currentRoll);
 
-    if (currentRoll === 1) {
-      rollArray = [];
+    if (currentRoll == 1) {
+      game.players[currentPlayer].rollArray = [];
+      if (currentPlayer < (playerCount - 1))
+        currentPlayer++;
+      else
+        currentPlayer = 0;
     }
-    var sum = rollArray.reduce((a, b) => a + b, 0);
+    curSum = game.players[currentPlayer].rollArray.reduce((a, b) => a + b, 0);
 
-    $("#output").text(sum);
-    $("#hold").click(function() {
-      $("#totalOutput").text(sum)
-        rollArray = []
+    $("#output").text(curSum);
+
+    console.log(currentRoll);
   });
-  console.log(currentRoll);
+
+  $("#hold").click(function() {
+      if (game.players[currentPlayer].name == "(none-set)")
+        game.players[currentPlayer] = new player($("#user-input").val(), (game.players[currentPlayer].tot + curSum));
+      else
+        game.players[currentPlayer] = new player(game.players[currentPlayer].name, (game.players[currentPlayer].tot + curSum));
+
+      var outStr = "";
+      for (i = 0; i < playerCount; i++){
+        if (i + 1 == playerCount)
+          outStr += game.players[i].name + " has scored " + game.players[i].tot;
+        else
+          outStr += game.players[i].name + " has scored " + game.players[i].tot + " | ";
+      }
+      $("#totalOutput").text(outStr);
+
+
+      if (currentPlayer < (playerCount - 1))
+        currentPlayer++;
+      else
+        currentPlayer = 0;
+      //currentPlayer++;
+      curSum = 0;
+      $("#output").text(curSum);
+      game.players[currentPlayer].rollArray = [];
   });
 });
